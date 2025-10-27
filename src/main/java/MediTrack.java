76741b13114e3bb6350a2e442620b0ba74ed2abc
normaharley <157118540+normaharley@users.noreply.github.com>
2025-10-27 14:21:47 +0100

    Create MediTrack.java

---
+++ src/main/java/MediTrack.java
@@ -0,0 +1,9 @@
+public class MediTrack {
+    public static String greet() {
+        return "Hello from MediTrack!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(greet());
+    }
+}
